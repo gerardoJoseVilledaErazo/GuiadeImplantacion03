86ef868786890355c0da217f56ab8a5389be43f2
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2021-09-17 10:37:14 -0600

    GuiadeImplantacion03

---
+++ application/controllers/Operaciones.php
@@ -0,0 +1,102 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Operaciones extends CI_Controller{
+    //
+    public function view( $page = "home" ){
+        //validamos
+        if(!is_file("application/views/pagina/" .$page . ".php")){
+            show_404();
+        }
+        $data['titulo'] = "Guia de Implantacion 03";
+
+        $this->load->view("plantilla/header", $data);
+        $this->load->view("pagina/" .$page, $data);//body
+        $this->load->view("plantilla/footer", $data);
+    }
+
+    public function sumar()
+    {
+        $data = array(
+            "titulo" => "GUÍA 03",
+            "resp" => "Error",
+        );
+        
+        if ($_POST) {
+            $num1 = $_POST["num1"];
+            $num2 = $_POST["num2"];
+            $valor = $num1 + $num2;
+            $data = array(
+                "titulo" => "GUÍA 03",
+                "resp" => $valor,
+            );
+        }
+        $this->load->view("plantilla/header", $data);
+        $this->load->view("plantilla/respuesta", $data);
+        $this->load->view("plantilla/footer", $data);
+    }
+    
+    public function restar()
+    {
+        $data = array(
+            "titulo" => "GUÍA 03",
+            "resp" => "Error",
+        );
+        if ($_POST) {
+            $num1 = $_POST["num1"];
+            $num2 = $_POST["num2"];
+            $valor = $num1 - $num2;
+            $data = array(
+                "titulo" => "GUÍA 03",
+                "resp" => $valor,
+            );
+        }
+        $this->load->view("plantilla/header", $data);
+        $this->load->view("plantilla/respuesta", $data);
+        $this->load->view("plantilla/footer", $data);
+    }
+
+    public function multiplicar()
+    {
+        $data = array(
+            "titulo" => "GUÍA 03",
+            "resp" => "Error",
+        );
+        if ($_POST) {
+            $num1 = $_POST["num1"];
+            $num2 = $_POST["num2"];
+            $num3 = $_POST["num3"];
+            $num4 = $_POST["num4"];
+            $num5 = $_POST["num5"];
+            $valor = $num1 * $num2 * $num3 * $num4 * $num5;
+            $data = array(
+                "titulo" => "GUÍA 03",
+                "resp" => $valor,
+            );
+        }
+        $this->load->view("plantilla/header", $data);
+        $this->load->view("plantilla/respuesta", $data);
+        $this->load->view("plantilla/footer", $data);
+    }
+
+    public function dividir()
+    {
+        $data = array(
+            "titulo" => "GUÍA 03",
+            "resp" => "Error",
+        );
+        if ($_POST) {
+            $num1 = $_POST["num1"];
+            $num2 = $_POST["num2"];
+            $num3 = $_POST["num3"];
+            $valor = ($num1 / $num2) / $num3;
+            $data = array(
+                "titulo" => "GUÍA 03",
+                "resp" => $valor,
+            );
+        }
+        $this->load->view("plantilla/header", $data);
+        $this->load->view("plantilla/respuesta", $data);
+        $this->load->view("plantilla/footer", $data);
+    }
+}
---
+++ application/views/plantilla/footer.php
@@ -0,0 +1,3 @@
+        <p><strong>Universidad de Sonsonate &copy; 2021</strong></p>
+    </body>
+</html>
